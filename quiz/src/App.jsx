f37a71ca1a17e7362e8581a9a94338e28430e7ee
Marko Cvetinovic <markocvetinovic030@gmail.com>
2023-05-03 13:43:57 +0200

    test

---
+++ quiz/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StartPage from './components/StartPage'
+function App() {
+
+  return (
+    <>
+      <StartPage></StartPage>
+    </>
+  )
+}
+
+export default App
---
+++ quiz/src/components/StartPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function StartPage() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default StartPage
