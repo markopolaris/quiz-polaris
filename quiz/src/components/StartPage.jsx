f334ae4a3319da56fce122ab81bfbdb21ed305d1
Marko Cvetinovic <markocvetinovic030@gmail.com>
2023-05-05 13:53:23 +0200

    odradjeno je da moze da se selektuje kategorija

--- quiz/src/components/StartPage.jsx
+++ quiz/src/components/StartPage.jsx
@@ -1,24 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-//treba da pozovem api ✅
-//treba da odradim duzinu pitanja ✅
-//treba da odredim kategorije koje mi trebaju ✅
-//treba da odredim tezinu pitanja ✅
-//treba da izdvojim sve parametre u nove nizove
+//odrediti kategorije ✅
+//napraviti novi niz gde ce da se dodaju i brisu kategorije koje ce da se dodaju u api ✅
+//ispisati sve to u "apiString" ✅
+//odrediti tezinu
+//napraviti novi niz gde da se dodaju tezine !MORA SAMO DA IMA JEDNA TEZINA
+//ispisati sve to u "apiString"
+//ispisati pitanja, i odgovore
+
+
+//api se cesto poziva kroz useEffect ali ne u nasem slucaju, jer nama komponenta treba da se renderuje kad se kliknestart dugme(moramo da napravimo obicnu funkciju)
 
 function StartPage() {
 
+const [selectedCategories, setSelectedCategories] = useState([]);
+const [selectDifficulty, setSelectDifficulty] = useState(['easy','medium','hard']);
+const [music, setMusic] = useState('music');
+const [history, setHistory] = useState('history');
+const [science, setScience] = useState('science');
+const [geography, setGeography] = useState('geography')
+let apiString = `https://the-trivia-api.com/v2/questions?limit=5&categories=${selectedCategories.join()}&difficulties=easy`;
 
-useEffect(() => {
-    fetch('https://the-trivia-api.com/v2/questions?limit=5&categories=music,history,science,geography&difficulties=easy,medium,hard')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('Došlo je do greške:', error);
-      });
-  }, []);
+
+
+function handleCategoryClick(category) {
+
+if (selectedCategories.includes(category)) {
+
+setSelectedCategories(selectedCategories.filter(cat => cat !== category));
+document.getElementById(category).classList.toggle('selected');
+} else {
+
+setSelectedCategories([...selectedCategories, category]);
+document.getElementById(category).classList.toggle('selected');
+}
+
+}
+
+
+function questions() {
+fetch(apiString)
+.then(response => response.json())
+.then(data => {
+console.log(data);
+})
+.catch(error => {
+console.error('Došlo je do greške:', error);
+});
+}
 
 
 
@@ -26,13 +55,28 @@ useEffect(() => {
 return (
 <section className='h-screen bg-[#1E293B] '>
     <div className='bg-[#334155] pl-36 pr-36 pt-20 pb-20 rounded-xl' id="box">
-        <div className='mb-16' id="categories">
-            <ul className='flex justify-between gap-20'>
-
-                <li className='text-white bg-[#475569] pl-10 pr-10 pb-4 pt-4 rounded-lg'>Music</li>
-                <li className='text-white bg-[#475569] pl-10 pr-10 pb-4 pt-4 rounded-lg'>History</li>
-                <li className='text-white bg-[#475569] pl-10 pr-10 pb-4 pt-4 rounded-lg'>Science</li>
-                <li className='text-white bg-[#475569] pl-10 pr-10 pb-4 pt-4 rounded-lg'>Geography</li>
+        <div className="mb-16" id="categories">
+            <ul className="flex justify-between gap-20">
+                <li className={`text-white ${ selectedCategories.includes('music') ? 'bg-black' : 'bg-[#475569]' } pl-10
+                    pr-10 pb-4 pt-4 rounded-lg`} onClick={()=> handleCategoryClick('music')}
+                    >
+                    Music
+                </li>
+                <li className={`text-white ${ selectedCategories.includes('history') ? 'bg-black' : 'bg-[#475569]' }
+                    pl-10 pr-10 pb-4 pt-4 rounded-lg`} onClick={()=> handleCategoryClick('history')}
+                    >
+                    History
+                </li>
+                <li className={`text-white ${ selectedCategories.includes('science') ? 'bg-black' : 'bg-[#475569]' }
+                    pl-10 pr-10 pb-4 pt-4 rounded-lg`} onClick={()=> handleCategoryClick('science')}
+                    >
+                    Science
+                </li>
+                <li className={`text-white ${ selectedCategories.includes('geography') ? 'bg-black' : 'bg-[#475569]' }
+                    pl-10 pr-10 pb-4 pt-4 rounded-lg`} onClick={()=> handleCategoryClick('geography')}
+                    >
+                    Geography
+                </li>
             </ul>
         </div>
 
@@ -44,19 +88,20 @@ return (
             </ul>
         </div>
 
-        <button className='pl-16 pr-16 pt-4 pb-4 block m-auto mt-20'>Start</button>
+        <button id='startButton' onClick={questions} className='pl-16 pr-16 pt-4 pb-4 block m-auto mt-20'>Start</button>
     </div>
 
     <div id="questionBox" className='bg-[#334155] pl-36 pr-36 pt-20 pb-20 rounded-xl mt-20'>
+        <div id="categoryName"></div>
         <div id="question" className='text-white bg-[#475569] w-9/10 h-40 m-auto'></div>
 
         <div id="grid" className='grid grid-cols-2 gap-10 mt-20'>
-        <div className="answers text-white bg-[#475569] h-20 w-48"></div>
-        <div className="answers text-white bg-[#475569] h-20 w-48"></div>
-        <div className="answers text-white bg-[#475569] h-20 w-48"></div>
-        <div className="answers text-white bg-[#475569] h-20 w-48"></div>
+            <div className="answers text-white bg-[#475569] h-20 w-48"></div>
+            <div className="answers text-white bg-[#475569] h-20 w-48"></div>
+            <div className="answers text-white bg-[#475569] h-20 w-48"></div>
+            <div className="answers text-white bg-[#475569] h-20 w-48"></div>
         </div>
-    </div> 
+    </div>
 </section>
 );
 }
